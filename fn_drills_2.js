26e23401861b13af856a43fa52b9b263e136396a
Michael Verdi <michael.v.verdi@gmail.com>
2019-03-26 15:24:36 -0400

    decode message function

--- fn_drills_2.js
+++ fn_drills_2.js
@@ -4,7 +4,7 @@ function jediName(firstName, lastName) {
   return `${lastName.slice(0,3)}${firstName.slice(0,2)}`
 }
 
-jediName("Beyonce", "Knowles")
+// jediName("Beyonce", "Knowles")
 
 function beyond(num) {
   if (num === Infinity || num === -Infinity) {
@@ -18,7 +18,28 @@ function beyond(num) {
   }
 }
 
-beyond(100)
+// beyond(100)
+
+function decode(word) {
+  switch(word[0]) {
+  case 'a': return word[1];
+  case 'b': return word[2];
+  case 'c': return word[3];
+  case 'd': return word[4];
+  default: return ' ';
+  }
+}
+
+function decodeMessage(sentence) {
+  return sentence
+    .split(" ")
+    .map(decode)
+    .join("")
+}
+
+// let secretMessage = 'craft block argon meter bells brown croon droop';
+// console.log(decodeMessage(secretMessage));
+
 
 
 
