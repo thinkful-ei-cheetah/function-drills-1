1a22884b497d57d8466cbc49ef7687d321b21b59
Michael Verdi <michael.v.verdi@gmail.com>
2019-03-26 14:29:33 -0400

    throw err if age is negative number

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 'use strict';
 
 function createGreeting(name, age){
-  return `Hi, my name is ${name} and I'm ${age} years old.  ` + 
-         `I was born in the year ${getYearOfBirth(age)}.`;
+  try {
+    return `Hi, my name is ${name} and I'm ${age} years old.  ` + 
+         `I was born in the year ${yearOfBirth(age)}.`;
+  } catch(e) {
+    return e.message
+  }
 }
 
-function getYearOfBirth(age){
+function yearOfBirth(age){
+  if (age < 0) {
+    throw new Error("age cannot be a negative number")
+  }
   return 2019 - age
 }
 
-let result = createGreeting("Michael", 29);
+let result = createGreeting("Michael", -2);
 console.log(result);
