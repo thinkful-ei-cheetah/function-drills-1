c791bf416da2a1845e74bd4f381147335c24f449
Michael Verdi <michael.v.verdi@gmail.com>
2019-03-26 14:48:32 -0400

    add type check for string and number...who knew that would be so difficult

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 'use strict';
 
 function createGreeting(name, age){
+  if (name === undefined || age === undefined) {
+    throw new Error('Arguments not valid');
+  }
+
+  if (typeof name !== 'string' || !age.toFixed) {
+    throw new TypeError();
+  }
+
   try {
     return `Hi, my name is ${name} and I'm ${age} years old.  ` + 
          `I was born in the year ${yearOfBirth(age)}.`;
   } catch(e) {
-    return e.message
+    return e.message;
   }
 }
 
 function yearOfBirth(age){
   if (age < 0) {
-    throw new Error("age cannot be a negative number")
+    throw new Error('age cannot be a negative number');
   }
-  return 2019 - age
+  return 2019 - age;
 }
 
-let result = createGreeting("Michael", -2);
+let result = createGreeting('Michael', 29);
 console.log(result);
