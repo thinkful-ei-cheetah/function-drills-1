3983a068308260446003d7f9b05b419fd4170dd4
Michael Verdi <michael.v.verdi@gmail.com>
2019-03-26 15:43:58 -0400

    added daysInMonth function

--- fn_drills_2.js
+++ fn_drills_2.js
@@ -40,6 +40,36 @@ function decodeMessage(sentence) {
 // let secretMessage = 'craft block argon meter bells brown croon droop';
 // console.log(decodeMessage(secretMessage));
 
+function daysInMonth(month, leapYear=false) {
+  const months = {
+    'January': 31,
+    'Febuary': 28,
+    'March': 31,
+    'April': 30,
+    'May': 31,
+    'June': 30,
+    'July': 31,
+    'August': 31,
+    'September': 30,
+    'October': 31,
+    'November': 30,
+    'December': 31
+  };
+
+  if (!months[month]) {
+    throw new Error('Must provide a valid month');
+  }
+
+  let days = months[month];
+  if (month === 'Febuary' && leapYear) {
+    days += 1;
+  }
+
+  return `${month} has ${days} days.`;
+}
+
+// console.log(daysInMonth('March'));
+
 
 
 
