4d8d4005b6c2ccd95630c65d8b6fe63c58285b5a
Michael Verdi <michael.v.verdi@gmail.com>
2019-03-26 14:19:19 -0400

    save result and console.log it

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function createGreeting(){
-  console.log(`Hi, my name is Michael and I'm 29 years old.`);
+  return `Hi, my name is Michael and I'm 29 years old.`;
 }
 
-createGreeting()
+let result = createGreeting();
+console.log(result);
